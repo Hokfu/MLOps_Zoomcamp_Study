40af935bbe45bd20391ab866c6abea01dc8091c8
Khant Htoo Naing <61989337+Hokfu@users.noreply.github.com>
2024-05-27 10:25:00 +0000

    module 2 homework

--- 02-experiment-tracking/module2_homework/train.py
+++ 02-experiment-tracking/module2_homework/train.py
@@ -1,9 +1,9 @@
 import os
 import pickle
 import click
-
+import mlflow
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
@@ -18,15 +18,20 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("module2_homework")
+    with mlflow.start_run():
+        mlflow.log_param("data_path", data_path)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
-
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        mlflow.log_param("max_depth", rf.max_depth)
+        mlflow.log_param("min_samples_split", rf.min_samples_split)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
